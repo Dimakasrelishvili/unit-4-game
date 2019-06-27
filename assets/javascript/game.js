8e587c5e01b1a4a7a7f23f52ccde21b2cfe9b2b3
Dimakasrelishvili <50391231+Dimakasrelishvili@users.noreply.github.com>
2019-06-27 14:01:34 -0400

    unit-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var crystal2 =0;
 var crystal3 =0;
 var crystal4 =0;
 var totalScore =0;
-var win =0;
+var win =0; 
 var loses =0;
 
 
@@ -17,27 +17,49 @@ crystal1 = Math.floor(Math.random()*12+1);
 crystal2 = Math.floor(Math.random()*12+1);
 crystal3 = Math.floor(Math.random()*12+1);
 crystal4 = Math.floor(Math.random()*12+1);
-console.log(mainNumber);
-console.log(crystal1)
 
-$("#crystal1").on("click", function(event) {
-    totalScore=totalScore+crystal1;
-    console.log(totalScore);
-
-
-});
-
-$("#crystal2").on("click", function(event) {
-    totalScore=totalScore+crystal2;
-});
-
-if(totalScore=mainNumber){
-    wins++;
-}
-if(totalScore>mainNumber){
-    loses++;
-}
-
+$(document).ready(function() {
+    
+     function main() {
+    if(totalScore==mainNumber){
+        win++;
+    };
+    if(totalScore>mainNumber){
+        loses++;
+         mainNumber =0;     
+         crystal1 =0;
+         crystal2 =0;
+         crystal3 =0;
+         crystal4 =0;
+         totalScore =0;
+         console.log(totalScore);
+         console.log(win);
+         console.log(losses);
+       
+       
+    };};
+$("#crystal1").on("click", function() {
+        main()
+        totalScore=totalScore+crystal1;
+    });
+    
+    $("#crystal2").on("click", function() {
+        totalScore=totalScore+crystal2;
+        main()
+    });
+    
+    $("#crystal3").on("click", function() {
+        totalScore=totalScore+crystal3;
+        main()
+        
+    });
+    $("#crystal4").on("click", function() {
+        totalScore=totalScore+crystal4;
+        main()
+    });
+    
+    
+}); 
 
 
 
